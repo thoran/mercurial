9ac016c39c6131bb6bfec3c02e5b819bbef71a1f
thoran <code@thoran.com>
2026-08-21 14:04:44 +1000

    0.13.0: Update the two vendored libraries which had drifted, and drop the twenty-two files nothing reaches.

--- lib/File/self.grep.rb
+++ lib/File/self.grep.rb
@@ -1,11 +1,16 @@
 # File/self.grep.rb
 # File.grep
 
-# 20260125
-# 0.0.0
+# 20260609
+# 0.0.1
 
 # Description: This returns a string (probably a number of lines) which is the result of grepping a file, similar to the grepping of a file on the command line.
 
+# Changes:
+# -/0: Include missing require.
+# 1. + require 'Regexp/to_regexp'
+
+require 'Regexp/to_regexp'
 require 'String/grep'
 require 'String/to_regexp'
 
